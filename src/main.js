3e74ffc1d13c59f3c6c0d096528b35b5bfd8f41b
liuxin <liux_0118@163.com>
2018-07-17 11:21:06 +0800

    添加sass

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+/* 导入vuex store */
+import store from './store'
+
 /* 导入scss */
-import './common/scss/index.scss'
+import 'common/scss/index.scss'
 
 Vue.config.productionTip = false
 
@@ -13,6 +16,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import * as actions from './actions'
 import * as getters from './getters'
-import state from 'state'
-import mutations from 'mutations'
+import state from './state'
+import mutations from './mutations'
 
 Vue.use(Vuex)
 
